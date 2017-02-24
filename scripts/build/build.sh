ea4b941f9421fe0c1c90e806b97cd45358b72a04
bergquist <carl.bergquist@gmail.com>
2017-02-24 09:22:57 +0100

    wip

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -7,19 +7,19 @@
 GOPATH=/go
 REPO_PATH=$GOPATH/src/github.com/grafana/grafana
 
-mkdir -p /go/src/github.com/grafana
-cd /go/src/github.com/grafana
+#mkdir -p /go/src/github.com/grafana
+#cd /go/src/github.com/grafana
 
-if [ "$CIRCLE_TAG" != "" ]; then
-  echo "Builing from tag $CIRCLE_TAG"
-  git clone https://github.com/grafana/grafana.git
-  cd $REPO_PATH
-  git checkout $CIRCLE_TAG
-else
-  echo "Building from branch $CIRCLE_BRANCH"
-  git clone --depth 1 https://github.com/grafana/grafana.git -b $CIRCLE_BRANCH
-  cd $REPO_PATH
-fi
+#if [ "$CIRCLE_TAG" != "" ]; then
+#  echo "Builing from tag $CIRCLE_TAG"
+#  git clone https://github.com/grafana/grafana.git
+#  cd $REPO_PATH
+#  git checkout $CIRCLE_TAG
+#else
+#  echo "Building from branch $CIRCLE_BRANCH"
+#  git clone --depth 1 https://github.com/grafana/grafana.git -b $CIRCLE_BRANCH
+#  cd $REPO_PATH
+#fi
 
 echo "current dir: $(pwd)"
 
--- scripts/build/deploy.sh
+++ scripts/build/deploy.sh
@@ -6,6 +6,7 @@ echo "Circle branch: ${CIRCLE_BRANCH}"
 echo "Circle tag: ${CIRCLE_TAG}"
 docker run -i -t --name gfbuild \
   -v $(pwd)/dist:/tmp/dist \
+  -v $(pwd):/go/src/github.com/grafana \
   -e "CIRCLE_BRANCH=${CIRCLE_BRANCH}" \
   -e "CIRCLE_TAG=${CIRCLE_TAG}" \
   -e "CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM}" \

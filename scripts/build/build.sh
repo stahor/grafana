9ec1704f7955ca9716dd25fbd07ab161fa8fb21a
bergquist <carl.bergquist@gmail.com>
2017-02-26 19:42:43 +0100

    wip

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -25,15 +25,15 @@ REPO_PATH=$GOPATH/src/github.com/grafana/grafana
 cd /go/src/github.com/grafana/grafana
 echo "current dir: $(pwd)"
 
-if [ "$CIRCLE_TAG" != "" ]; then
-  echo "Building a release from tag $CIRCLE_TAG"
-  go run build.go -buildNumber=${CIRCLE_BUILD_NUM} -includeBuildNumber=false build
-else
-  echo "Building incremental build for $CIRCLE_BRANCH"
-  go run build.go -buildNumber=${CIRCLE_BUILD_NUM} build
-fi
+#if [ "$CIRCLE_TAG" != "" ]; then
+#  echo "Building a release from tag $CIRCLE_TAG"
+#  go run build.go -buildNumber=${CIRCLE_BUILD_NUM} -includeBuildNumber=false build
+#else
+#  echo "Building incremental build for $CIRCLE_BRANCH"
+#  go run build.go -buildNumber=${CIRCLE_BUILD_NUM} build
+#fi
 
-yarn install --pure-lockfile --no-progress
+#yarn install --pure-lockfile --no-progress
 
 source /etc/profile.d/rvm.sh
 rvm use 2.1.9 --default
